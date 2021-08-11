5b7317731c6c9e5dde65b082be8dcf02bf85e2b5
AlexLa00 <N01313354@humbermail.ca>
2021-08-11 05:27:20 -0400

    init

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'About us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Features',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Quick Guide',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
